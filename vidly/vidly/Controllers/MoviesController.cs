e1ac3f6150ff36663ac339be592f41cd082cfb1b
abharana24 <abha801@gmail.com>
2020-03-26 22:37:26 +0530

    movies controller  with custom  routes

---
+++ vidly/vidly/Controllers/MoviesController.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using vidly.Models;
+
+namespace vidly.Controllers
+{
+    public class MoviesController : Controller
+    {
+        // GET: Movies/random
+        public ActionResult Random()
+        {
+            var movie = new Movie()
+            {
+                Name = "Shrek!"
+            };
+            return View(movie);
+        }
+
+        public ActionResult Edit(int id)
+        {
+            return Content("id=" + id);
+        }
+
+        public ActionResult Index( int? pageIndex, string sortBy)
+        {
+            if(!pageIndex.HasValue)
+                pageIndex = 1;
+            if (string.IsNullOrWhiteSpace(sortBy))
+                sortBy = "Name";
+            return Content(String.Format("pageIndex={0}&sortBy={1}",pageIndex,sortBy));
+        }
+
+        public ActionResult ByReleaseDate(int year, int month)
+        {
+            return Content(year + "/" + month);
+        }
+    }
+}
