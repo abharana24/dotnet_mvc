e3d580b0cfe84b8cb5151116ff4bc7d3d628f9b7
abharana24 <abha801@gmail.com>
2020-03-27 01:20:00 +0530

    second module complete with viewmodels and partial views

--- vidly/vidly/Controllers/MoviesController.cs
+++ vidly/vidly/Controllers/MoviesController.cs
@@ -1,41 +1,45 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Web;
+﻿using System.Collections.Generic;
 using System.Web.Mvc;
 using vidly.Models;
+using vidly.ViewModels;
 
-namespace vidly.Controllers
+namespace Vidly.Controllers
 {
     public class MoviesController : Controller
     {
-        // GET: Movies/random
+        public ViewResult Index()
+        {
+            var movies = GetMovies();
+
+            return View(movies);
+        }
+
+        private IEnumerable<Movie> GetMovies()
+        {
+            return new List<Movie>
+            {
+                new Movie { Id = 1, Name = "Shrek" },
+                new Movie { Id = 2, Name = "Wall-e" }
+            };
+        }
+
+        // GET: Movies/Random
         public ActionResult Random()
         {
-            var movie = new Movie()
+            var movie = new Movie() { Name = "Shrek!" };
+            var customers = new List<Customer>
             {
-                Name = "Shrek!"
+                new Customer { Name = "Customer 1" },
+                new Customer { Name = "Customer 2" }
             };
-            return View(movie);
-        }
 
-        public ActionResult Edit(int id)
-        {
-            return Content("id=" + id);
-        }
+            var viewModel = new RandomMovieViewModel
+            {
+                Movie = movie,
+                Customers = customers
+            };
 
-        public ActionResult Index( int? pageIndex, string sortBy)
-        {
-            if(!pageIndex.HasValue)
-                pageIndex = 1;
-            if (string.IsNullOrWhiteSpace(sortBy))
-                sortBy = "Name";
-            return Content(String.Format("pageIndex={0}&sortBy={1}",pageIndex,sortBy));
-        }
-
-        public ActionResult ByReleaseDate(int year, int month)
-        {
-            return Content(year + "/" + month);
+            return View(viewModel);
         }
     }
 }
