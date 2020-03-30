8cfb10fa3e9beced10ba23fc2a8d30fecd7e55e5
abharana24 <abha801@gmail.com>
2020-03-30 05:32:56 +0530

    validations on customer forms implemented

--- vidly/vidly/Controllers/CustomersController.cs
+++ vidly/vidly/Controllers/CustomersController.cs
@@ -27,6 +27,7 @@ namespace vidly.Controllers
             var membershipTypes = _context.MembershipTypes.ToList();
             var viewModel = new CustomerFormViewModel
             {
+                Customer = new Customer(),
                 MembershipTypes = membershipTypes
             };
 
@@ -34,9 +35,20 @@ namespace vidly.Controllers
         }
 
         [HttpPost]
+        [ValidateAntiForgeryToken]
         public ActionResult Save(Customer customer)
         {
-            if (customer.Id == 0)
+            if (!ModelState.IsValid)
+            {
+                var viewModel = new CustomerFormViewModel
+                {
+                    Customer = customer,
+                    MembershipTypes = _context.MembershipTypes.ToList()
+                };
+
+                return View("CustomerForm", viewModel);
+            }
+                if (customer.Id == 0)
                 _context.Customers.Add(customer);
             else
             {
--- vidly/vidly/Models/Customer.cs
+++ vidly/vidly/Models/Customer.cs
@@ -15,6 +15,7 @@ namespace vidly.Models
         public string Name { get; set; }
 
         [Display(Name = "Date of Birth")]
+        [Min18YearsIfAMember]
         public DateTime? BirthDate { get; set; }
         public bool IsSubscribedToNewsletter { get; set; }
         public MembershipType MembershipType { get; set; }
